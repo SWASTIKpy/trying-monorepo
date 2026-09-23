d6bbf105292050d70db4f03d4d52e54307945f1d
swastikPY <swastikvishwa908@gmail.com>
2026-09-23 17:06:56 +0530

    added turborepo

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -3,12 +3,12 @@ import { formatCurrency } from "@monorepo/utils";
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT =  3000;
 
 app.get("/", (req, res) => {
-    const str = formatCurrency(1234.56, "USD");
+    const formattedCurrency = formatCurrency(1234.56, "USD");
   return res.json({
-    
+      formattedCurrency
   });
 });
 
